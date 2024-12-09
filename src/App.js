bbdbbbaed26cfbbc2e0a56b369ee83d57aadcb81
Shaktisen Chaudhary <shaktisenchaudhary@Shaktisens-MacBook-Air.local>
2024-12-09 12:39:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MyFile from './components/MyFile';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MyFile/>
+    </div>
+  );
+}
+
+export default App;
